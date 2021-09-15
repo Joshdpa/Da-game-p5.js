0ab8bf63eae203123d236ca21ee8cbe9c1b09b31
Joshua Allen <90309098+Joshdpa@users.noreply.github.com>
2021-09-15 08:05:40 +0000

    monke

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-var x, v;
+var ball1, ball2, ball3
+
+class Ball{
+  constructor(x, y, vx, vy, h, w, c){
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.h = h;
+  this.w = w;
+  this.c = c;
+  }
+
+  draw(){
+    fill(this.color);
+    ellipse(this.x,this.y,this.w,this.h)
+    this.x = 
+  }
+}
+
 function setup() {
-  createCanvas(500, 400);
-  x = 30;
-  v = 5;
+  createCanvas(400, 300);
+
+  ball1 = new Ball(30,200,50,50,5,5,"green")
+
+  ball2 = new Ball(120,210,50,50,3,3,"black")
+
+  ball3 = new Ball(200,120,50,50,1,1,"blue")
+
 }
 
-var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
-
-function draw() {
-  background(120)
-  circle(x,200,30);
-  x = x + v;
-  if (x < 30){
-    v = 5;
-  }
-
-  if (x > 470){
-    v = -5;
-  }
-
-
-  
-}
